50685ab0c446287fcac67debb9acbe4e5538acf2
ShinaOdukoya <shinaodukoya2@gmail.com>
2020-03-20 14:55:22 +0100

    Project set up for GoodTime

---
+++ goodTime/src/main/java/com/GoodTime/goodtime/GoodtimeApplication.java
@@ -0,0 +1,13 @@
+package com.GoodTime.goodtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodtimeApplication.class, args);
+	}
+
+}
---
+++ goodTime/src/test/java/com/GoodTime/goodtime/GoodtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GoodTime.goodtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
